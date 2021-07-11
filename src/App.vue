2ddc4f60312da26ffa8b0d9929487e500149e50f
Yuki <y.tsuruhara@leihauoli.com>
2021-07-11 22:39:51 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,37 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link><br>
+      {{ $store.state.count }}
     </div>
     <router-view/>
+    <button @click="setLogin">login</button>
+    <br>
+    <ul>
+      <li v-for="user in visibleUsers" :key="user.id">
+        {{ user.id }} : {{ user.name }} : {{ user.isVisible }}
+      </li>
+    </ul><br>
+    {{ getUserById.name }}
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapActions } from 'vuex'
+export default {
+  methods:{
+    ...mapActions('auth',['setLoginUser']),
+    setLogin(){
+      this.setLoginUser({name: '大谷'})
+    }
+  },
+  computed:{
+    visibleUsers(){
+      return this.$store.getters.visibleUsers
+    },
+    getUserById(){
+      return this.$store.getters.getUserById(3)
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: { //初期値
+    count:0,
+    users:[
+      {id:1,name:'大谷',isVisible:true},
+      {id:2,name:'筒香',isVisible:true},
+      {id:3,name:'坂本',isVisible:true}
+    ]
   },
-  mutations: {
+  getters:{
+    visibleUsers:state => state.users.filter(user => user.isVisible),
+    getUserById: state => id => {
+      return state.users.find(user => user.id === id)
+    }
+  },
+  mutations: {//stateの状態を変更
+    increment(state){
+      state.count ++
+    },
+    addCount(state,payload){
+      state.count += payload.value//基本的にオブジェクトで渡す
+    }
   },
   actions: {
+    incrementAction({ commit }){
+      commit('increment')
+    },
+    addCountAction({ commit },payload){
+      commit('addCount',payload)
+    }
   },
   modules: {
+    auth
   }
 })
